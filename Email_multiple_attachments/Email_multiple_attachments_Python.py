a544a03262c4ba4c3d1983f0672f430be009971b
vevina233 <75918644+vevina233@users.noreply.github.com>
2020-12-14 14:38:17 +1300

    Add description

--- Email_multiple_attachments/Email_multiple_attachments_Python.py
+++ Email_multiple_attachments/Email_multiple_attachments_Python.py
@@ -30,6 +30,8 @@ def sendmail(receiver, attachment, subject):
     mail.To = receiver
     mail.subject = sub.encode('utf-8').decode('utf-8')
     mail.Body = body.encode('utf-8').decode('utf-8')
+    
+    # attach multiple files in an individual email
     for i in range(len(attachment)):
         mail.Attachments.Add(attachment[i])
     mail.Send()
@@ -40,17 +42,17 @@ path = input('Path: ')
 addr_path = input('Path of Address: ')
 addr = pd.read_excel(addr_path)
 
+# address df columns: "Code" - code for client/supplier; "email": email address
 for j in range(len(addr.Code)):
     dirlist = []
     sub_code = addr.Code[j].astype("str")
     subjects = sub_code + " - " + month
     for dirpath,dirname,filename in os.walk(path):
-        print(addr.Code[j].astype("str"))
+        # find all files with the first 4 letters that match the code of each receiver
         for i in filename:
             if i[0:4] == sub_code:
-                dirlist.append(os.path.join(dirpath,i))
-                
-    sendmail(addr["add"][j], dirlist, subjects)
+                dirlist.append(os.path.join(dirpath,i))            
+    sendmail(addr["email"][j], dirlist, subjects)
 
 
 
